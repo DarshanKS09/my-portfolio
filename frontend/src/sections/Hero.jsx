e219003de08a8d956d5f025261dfe27e26f07d6b
Darsh <darshans.22.beis@acharya.ac.in>
2026-01-31 19:41:30 +0530

    last commit??

--- frontend/src/sections/Hero.jsx
+++ frontend/src/sections/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
   <img
     src={profileImg}
     alt="DKS"
-    className="w-54 h-54 rounded-full border-2 border-red-600 object-cover"
+    className="w-54 h-48 rounded-full border-2 border-red-600 object-cover"
   />
 </div>
                 </div>
