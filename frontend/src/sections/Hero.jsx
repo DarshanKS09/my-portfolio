b32ae7ee1f804244de94245b8a2791164c1b54d4
DarshanKS09 <darshanksgowda09@gmail.com>
2026-03-11 12:10:13 +0530

    resume button fixed

--- frontend/src/sections/Hero.jsx
+++ frontend/src/sections/Hero.jsx
@@ -111,7 +111,7 @@ const Hero = () => {
             {/* CTA Buttons */}
             <motion.div variants={fadeInUp} className="pt-4 flex gap-4">
              <a
-  href="/public/Resume.pdf"
+  href="/Resume.pdf"
   target="_blank"
   rel="noopener noreferrer"
 >
